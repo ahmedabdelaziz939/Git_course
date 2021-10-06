3887a40804cb5638a8dc898ccd1295ed080ddc86
ahmedabdelaziz939 <ahmedabdelaziz098a@gmail.com>
2021-10-06 03:30:47 +0300

    changing the method of class

--- calc/lib/calc.dart
+++ calc/lib/calc.dart
@@ -1,47 +1,50 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  Calc();
-}
-
-class Calc {
-  Calc() {
-    bool run = true;
-    while (run) {
-      var num1 = int.parse(stdin.readLineSync() ?? "");
-      var op = stdin.readLineSync();
-      var num2 = int.parse(stdin.readLineSync() ?? "");
-      if (num1 == 0) {
-        print('exited');
-        break;
-      }
+void main() {
+  Calc calculator = Calc();
+  bool run = true;
+  while (run) {
+    var num1 = int.parse(stdin.readLineSync()??"");
+    var op = stdin.readLineSync();
+    var num2 = int.parse(stdin.readLineSync()??"");
+    if (num1 == 0) {
+      print('exited');
+    } else {
       switch (op) {
         case '+':
-          print('$num1 + $num2 = ${num1 + num2}');
-          run = false;
+          print(calculator.Add(num1, num2));
           break;
 
         case '-':
-          print('$num1 - $num2 = ${num1 - num2}');
+          print(calculator.Sub(num1, num2));
           break;
 
         case '*':
-          print('$num1 * $num2 = ${num1 * num2}');
+          print(calculator.Mult(num1, num2));
           break;
 
         case '/':
-          print('$num1 / $num2 = ${num1 / num2}');
-          break;
-
-        case '%':
-          print('$num1 % $num2 = ${num1 % num2}');
-          break;
-
-        default:
-          // If the operator is other than +, -, * or /, error message is shown
-          print("Error! operator is not correct");
+          print(calculator.Divid(num1, num2));
           break;
       }
     }
+  }
+}
+
+class Calc {
+  int Add(num1, num2) {
+    return num1 + num2;
+  }
+
+  int Sub(num1, num2) {
+    return num1 - num2;
+  }
+
+  int Mult(num1, num2) {
+    return num1 * num2;
+  }
+
+  double Divid(num1, num2) {
+    return num1 / num2;
   }
 }
